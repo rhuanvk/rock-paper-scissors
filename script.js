604cf0deea695976f6bd9c310d2831ebb48328ff
rhuanvk <rhuanvk@gmail.com>
2023-02-08 17:04:35 -0300

    rename some vars

--- script.js
+++ script.js
@@ -1,64 +1,64 @@
-// Registra a escolha do usuário e chama a função que inicia o jogo
 const choices = ["Rock", "Paper", "Scissors"];
 const result = document.querySelector("#result");
 const scoreboard = document.querySelector("#scoreboard");
-
 const buttons = document.querySelectorAll("button");
 
-// Faz a jogada do PC e registra os pontos
-let pScore = 0;
-let cScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 buttons.forEach((button) =>
-   button.addEventListener("click", function (e) {
-      // Check if the game has already been won
-      if (pScore >= 5 || cScore >= 5) {
-         return;
-      }
-      playGame(choices[button.id]);
-      showScore();
-   })
+    button.addEventListener("click", function(e) {
+        // Check if the game has already been won
+        if (playerScore >= 5 || computerScore >= 5) {
+            return;
+        }
+        // If not, call the function to play the round and show score
+        playRound(choices[button.id]);
+        showScore();
+    })
 );
 
-function playGame(pChoice) {
-   const rChoice = Math.floor(Math.random() * 3);
-   const cChoice = choices[rChoice];
-   if (pChoice === cChoice) {
-      result.innerHTML = "It's a draw. Let's play again!";
-   } else if (
-      (pChoice == "Rock" && cChoice == "Scissors") ||
-      (pChoice == "Paper" && cChoice == "Rock") ||
-      (pChoice == "Scissors" && cChoice == "Paper")
-   ) {
-      result.innerHTML = `Computer chose ${cChoice}.<br /> <span class="player-color">Player chose ${pChoice}.</span><br /> ${pChoice} beats ${cChoice}. You score! `;
-      pScore++;
-   } else {
-      result.innerHTML = `Computer chose ${cChoice}.<br /> <span class="player-color"> Player chose ${pChoice}.</span><br /> ${cChoice} beats ${pChoice}. Computer score!`;
-      cScore++;
-   }
+// Play the round accordingly the pressed button
+function playRound(playerChoice) {
+    // Generate a random choice for computer
+    const randomNum = Math.floor(Math.random() * 3);
+    const computerChoice = choices[randomNum];
+    if (playerChoice === computerChoice) {
+        result.innerHTML = "It's a tie. Let's play again!";
+    }
+    else if (
+        (playerChoice == "Rock" && computerChoice == "Scissors") ||
+        (playerChoice == "Paper" && computerChoice == "Rock") ||
+        (playerChoice == "Scissors" && computerChoice == "Paper")
+    ) {
+        result.innerHTML = `Computer chose ${computerChoice}.<br /> <span class="player-color">Player chose ${playerChoice}.</span><br /> ${playerChoice} beats ${computerChoice}. You score!`;
+        playerScore++;
+    }
+    else {
+        result.innerHTML = `Computer chose ${computerChoice}.<br /> <span class="player-color"> Player chose ${playerChoice}.</span><br /> ${computerChoice} beats ${playerChoice}. Computer score!`;
+        computerScore++;
+    }
 }
 
-// Mostra o placar e encerra o jogo quando alguém alcançar 5 vitórias
+// Show scoreboard and finish the game when someone gets 5 points
 function showScore() {
-   for (let i = 0; i < 5; i++) {
-      scoreboard.innerHTML = `Score: <br /> <span class="game-score"> ${pScore} :  ${cScore} </span> `;
-   }
-   if (pScore === 5) {
-      scoreboard.innerHTML +=
-         "<br /> <p>Congratulations, <span class='player-wins'>you win!</span></p>";
-   } else if (cScore === 5) {
-      scoreboard.innerHTML +=
-         "<br /> <p>Game over! <span class='computer-wins'>Computer wins.</span></p>";
-   }
-   if (pScore === 5 || cScore === 5) {
-      scoreboard.innerHTML +=
-         '<button type="button" id="reset" onclick="resetScore()">Play again</button>';
-   }
+    for (let i = 0; i < 5; i++) {
+        scoreboard.innerHTML = `Score: <br /> <span class="game-score"> ${playerScore} :  ${computerScore} </span> `;
+    }
+    if (playerScore === 5) {
+        scoreboard.innerHTML += "<br /> <p>Congratulations, <span class='player-wins'>you win!</span></p>";
+    }
+    else if (computerScore === 5) {
+        scoreboard.innerHTML += "<br /> <p>Game over! <span class='computer-wins'>Computer wins.</span></p>";
+    }
+    if (playerScore === 5 || computerScore === 5) {
+        scoreboard.innerHTML += '<button type="button" id="reset" onclick="resetScore()">Play again</button>';
+    }
 }
 
 function resetScore() {
-   result.innerHTML = "";
-   scoreboard.innerHTML = "";
-   pScore = 0;
-   cScore = 0;
+    result.innerHTML = "";
+    scoreboard.innerHTML = "";
+    playerScore = 0;
+    computerScore = 0;
 }
